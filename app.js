db3cad8af3d27e9912e9a518ae3fd6c91aa82db5
Aryan-Kanak <54687924+Aryan-Kanak@users.noreply.github.com>
2022-08-26 12:30:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -44,48 +44,59 @@ function getCode(){
 }
 
 function fetchAccessToken( code ){
-    let body = "grant_type=authorization_code";
-    body += "&code=" + code; 
-    body += "&redirect_uri=" + encodeURI(redirect_uri);
-    body += "&client_id=" + client_id;
-    body += "&client_secret=" + client_secret;
-    callAuthorizationApi(body);
+    $.ajax({
+        type: "POST",
+        url: "https://accounts.spotify.com/api/token",
+        headers: {
+            'Authorization': 'Basic ' + btoa(client_id + ":" + client_secret),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        success: handleAuthorizationResponse,
+        error: errorFunction,
+        data: {
+            grant_type: "authorization_code",
+            code: code,
+            redirect_uri: encodeURI(redirect_uri),
+            client_id: client_id,
+            client_secret: client_secret
+        }
+    });
 }
 
 function refreshAccessToken(){
     refresh_token = sessionStorage.getItem("refresh_token");
-    let body = "grant_type=refresh_token";
-    body += "&refresh_token=" + refresh_token;
-    body += "&client_id=" + client_id;
-    callAuthorizationApi(body);
+    $.ajax({
+        type: "POST",
+        url: "https://accounts.spotify.com/api/token",
+        headers: {
+            'Authorization': 'Basic ' + btoa(client_id + ":" + client_secret),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        success: handleAuthorizationResponse,
+        error: errorFunction,
+        data: {
+            grant_type: "refresh_token",
+            refresh_token: refresh_token,
+            client_id: client_id
+        }
+    });
 }
 
-function callAuthorizationApi(body){
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://accounts.spotify.com/api/token", true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
-    xhr.onload = handleAuthorizationResponse;
-    xhr.send(body);
+function handleAuthorizationResponse(response){
+    if ( response.access_token != undefined ){
+        access_token = response.access_token;
+        sessionStorage.setItem("access_token", access_token);
+    }
+    if ( response.refresh_token  != undefined ){
+        refresh_token = response.refresh_token;
+        sessionStorage.setItem("refresh_token", refresh_token);
+    }
+    onPageLoad();
 }
 
-function handleAuthorizationResponse(){
-    if ( this.status == 200 ){
-        var data = JSON.parse(this.responseText);
-        if ( data.access_token != undefined ){
-            access_token = data.access_token;
-            sessionStorage.setItem("access_token", access_token);
-        }
-        if ( data.refresh_token  != undefined ){
-            refresh_token = data.refresh_token;
-            sessionStorage.setItem("refresh_token", refresh_token);
-        }
-        onPageLoad();
-    }
-    else {
-        console.log(this.responseText);
-        alert(this.responseText);
-    }
+function errorFunction() {
+    console.log(response);
+    alert(response);
 }
 
 function requestAuthorization(){
@@ -101,94 +112,84 @@ function requestAuthorization(){
 }
 
 function callApi(method, url, body, callback){
-    let xhr = new XMLHttpRequest();
-    xhr.open(method, url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-    xhr.onload = callback;
-    xhr.send(body);
+    $.ajax({
+        type: method,
+        url: url,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + access_token
+        },
+        success: callback,
+        error: errorFunction,
+        statusCode: {
+            401: function() {
+                refreshAccessToken();
+                callback();
+            }
+        },
+        data: body
+    });
 }
 
 // search for a track by name
 function search() {
     let trackName = document.getElementById("search").value;
     callApi("GET", 'https://api.spotify.com/v1/search?q=' + trackName + '&type=track', null,
-    function() {
-        if ( this.status == 200 ){
-            $('#get-features').show();
-            let tracks = JSON.parse(this.responseText).tracks.items;
-            let menu = document.getElementById("results");
-            // remove all current options in dropdown menu
-            while(menu.firstChild) {
-                menu.removeChild(menu.firstChild);
-            }
-            // add results of search to dropdown menu
-            for (let i = 0; i < tracks.length; ++i) {
-                let opt = document.createElement('option');
-                opt.value = tracks[i].id;
-                opt.innerHTML = tracks[i].name + '-' + tracks[i].artists[0].name;
-                menu.appendChild(opt);
-            }
+    function(response) {
+        $('#get-features').show();
+        let tracks = response.tracks.items;
+        let menu = document.getElementById("results");
+        // remove all current options in dropdown menu
+        while(menu.firstChild) {
+            menu.removeChild(menu.firstChild);
         }
-        else if ( this.status == 401 ){
-            refreshAccessToken();
-            search();
+        // add results of search to dropdown menu
+        for (let i = 0; i < tracks.length; ++i) {
+            let opt = document.createElement('option');
+            opt.value = tracks[i].id;
+            opt.innerHTML = tracks[i].name + '-' + tracks[i].artists[0].name;
+            menu.appendChild(opt);
         }
-        else {
-            console.log(this.responseText);
-            alert(this.responseText);
-        }
-    })
+    });
 }
 
 // get features for a track
 function getFeatures() {
     let trackId = document.getElementById("results").value;
     callApi("GET", "https://api.spotify.com/v1/audio-features/" + trackId, null,
-    function() {
-        if ( this.status == 200 ){
-            let data = JSON.parse(this.responseText);
-            // set key
-            if (data.key == 0) {
-                data.key = "C";
-            } else if (data.key == 1) {
-                data.key = "C♯/D♭";
-            } else if (data.key == 2) {
-                data.key = "D";
-            } else if (data.key == 3) {
-                data.key = "D♯/E♭";
-            } else if (data.key == 4) {
-                data.key = "E";
-            } else if (data.key == 5) {
-                data.key = "F";
-            } else if (data.key == 6) {
-                data.key = "F♯/G♭";
-            } else if (data.key == 7) {
-                data.key = "G";
-            } else if (data.key == 8) {
-                data.key = "G♯/A♭";
-            } else if (data.key == 9) {
-                data.key = "A";
-            } else if (data.key == 10) {
-                data.key = "A♯/B♭";
-            } else if (data.key == 11) {
-                data.key = "B";
-            } else if (data.key == -1) {
-                data.key = "None";
-            }
-            // set mode
-            data.mode = data.mode ? "Major" : "Minor";
-            let featuresSource = document.getElementById("features-template").innerHTML;
-            let featuresTemplate = Handlebars.compile(featuresSource);
-            document.getElementById("features").innerHTML = featuresTemplate(data);
+    function(response) {
+        // set key
+        if (response.key == 0) {
+            response.key = "C";
+        } else if (response.key == 1) {
+            response.key = "C♯/D♭";
+        } else if (response.key == 2) {
+            response.key = "D";
+        } else if (response.key == 3) {
+            response.key = "D♯/E♭";
+        } else if (response.key == 4) {
+            response.key = "E";
+        } else if (response.key == 5) {
+            response.key = "F";
+        } else if (response.key == 6) {
+            response.key = "F♯/G♭";
+        } else if (response.key == 7) {
+            response.key = "G";
+        } else if (response.key == 8) {
+            response.key = "G♯/A♭";
+        } else if (response.key == 9) {
+            response.key = "A";
+        } else if (response.key == 10) {
+            response.key = "A♯/B♭";
+        } else if (response.key == 11) {
+            response.key = "B";
+        } else if (response.key == -1) {
+            response.key = "None";
         }
-        else if ( this.status == 401 ){
-            refreshAccessToken();
-            search();
-        }
-        else {
-            console.log(this.responseText);
-            alert(this.responseText);
-        }
-    })
+        // set mode
+        response.mode = response.mode ? "Major" : "Minor";
+        let featuresSource = document.getElementById("features-template").innerHTML;
+        let featuresTemplate = Handlebars.compile(featuresSource);
+        document.getElementById("features").innerHTML = featuresTemplate(response);
+    });
 }
