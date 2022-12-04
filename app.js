203b3b9b4fc004aa532456fb8ebc8c94e6d0040d
Aryan-Kanak <54687924+Aryan-Kanak@users.noreply.github.com>
2022-12-04 15:45:26 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var redirect_uri = "https://aryan-kanak.github.io/spotify-track-features";
+var redirect_uri = "http://127.0.0.1:5500/index.html";
 var client_id = "cbfc4d354b014617b66a9f3225156354";
 var client_secret = "7cd9e2657ff24a09acd76fd5eabdd3e4";
 
@@ -24,6 +24,28 @@ function onPageLoad(){
             $('#login').hide();
             $('#stats').show();
         }
+    }
+}
+
+function fillHelpText () {
+    let helpText = [
+        "A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic.",
+        "Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable.",
+        "Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity.",
+        "Predicts whether a track contains no vocals. \"Ooh\" and \"aah\" sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly \"vocal\". The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0.",
+        "The key the track is in.",
+        "Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live.",
+        "The overall loudness of a track in decibels (dB).",
+        "Mode indicates the modality (major or minor) of a track, the type of scale from which its melodic content is derived.",
+        "Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value.",
+        "The overall estimated tempo of a track in beats per minute (BPM).",
+        "An estimated time signature.",
+        "A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track."
+    ]
+
+    let helpIcons = document.getElementsByClassName("help-text")
+    for (let i = 0; i < helpIcons.length; ++i) {
+        helpIcons[i].setAttribute("title", helpText[i])
     }
 }
 
@@ -191,5 +213,6 @@ function getFeatures() {
         let featuresSource = document.getElementById("features-template").innerHTML;
         let featuresTemplate = Handlebars.compile(featuresSource);
         document.getElementById("features").innerHTML = featuresTemplate(response);
+        fillHelpText();
     });
 }
---
+++ config.js
@@ -0,0 +1,4 @@
+var config = {
+    CLIENT_ID : "cbfc4d354b014617b66a9f3225156354",
+    CLIENT_SECRET : "7cd9e2657ff24a09acd76fd5eabdd3e4"
+}
